f0b5ab3a44a11e28823bd1c206b7880b31c070d3
Mina Farid <mina.farid@uwaterloo.ca>
2018-11-09 15:42:35 -0500

    modify test_holocelan.py

--- tests/test_holoclean.py
+++ tests/test_holoclean.py
@@ -2,25 +2,30 @@ import holoclean
 
 from holoclean.detect import NullDetector, ViolationDetector
 from holoclean.repair.featurize import InitFeaturizer
+from holoclean.repair.featurize import InitAttFeaturizer
+from holoclean.repair.featurize import InitSimFeaturizer
+from holoclean.repair.featurize import FreqFeaturizer
 from holoclean.repair.featurize import OccurFeaturizer
 from holoclean.repair.featurize import ConstraintFeat
+from holoclean.repair.featurize import LangModelFeat
 
 def get_tid(row):
-    return row['tupleid'] - 1
+    return row['tid'] - 1
 
 def get_attr(row):
     return row['attribute'].lower()
 
 def get_value(row):
-    return row['correct_value'].lower()
+    return row['correct_val'].lower()
 
-hc = holoclean.HoloClean(pruning_topk=0.3, epochs=30, momentum=0.0, l=0.001, weight_decay=0.9, threads=20, batch_size=1, normalize=True, verbose=True, bias=True, timeout=3 * 60000).session
-hc.load_data('hospital', 'data', 'hospital.csv')
-hc.load_dcs('data', 'hospital_constraints_att.txt')
+hc = holoclean.HoloClean(pruning_topk=0.1, epochs=30, weight_decay=0.01, threads=20, batch_size=1, verbose=True, timeout=3*60000).session
+hc.load_data('hospital','data','hospital.csv')
+hc.load_dcs('data','hospital_constraints_att.txt')
 hc.ds.set_constraints(hc.get_dcs())
-detectors = [NullDetector(), ViolationDetector()]
+detectors = [NullDetector(),ViolationDetector()]
 hc.detect_errors(detectors)
 hc.setup_domain()
-featurizers = [InitFeaturizer(), OccurFeaturizer(), ConstraintFeat()]
+featurizers = [InitAttFeaturizer(), InitSimFeaturizer(), FreqFeaturizer(), OccurFeaturizer(), LangModelFeat(), ConstraintFeat()]
 hc.repair_errors(featurizers)
-hc.evaluate('data', 'hospital_clean.csv', get_tid, get_attr, get_value)
+hc.evaluate('data','hospital_clean.csv', get_tid, get_attr, get_value)
+
