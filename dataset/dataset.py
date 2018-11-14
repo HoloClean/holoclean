450808c9e3d0866bf2f6bf22bfab8c7c540f2017
Richard Wu <richardwu1997@gmail.com>
2018-11-14 01:44:57 -0500

    Added docstrings and comments to better explain repair process and domain generation.

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,6 +6,15 @@ from .table import Table, Source
 
 
 def dictify(frame):
+    """
+    dictify converts a frame with columns
+
+      col1    | col2    | .... | coln   | value
+      ...
+    to a dictionary that maps values valX from colX
+
+    { val1 -> { val2 -> { ... { valn -> value } } } }
+    """
     d = {}
     for row in frame.values:
         here = d
@@ -56,24 +65,43 @@ class Dataset:
 
     # Fixed to load data from a CSV file at the moment.
     def load_data(self, name, f_path, f_name, na_values=None):
+        """
+        load_data takes a CSV file of the initial data, adds tuple IDs (_tid_)
+        to each row to uniquely identify an 'entity', and generates unique
+        index numbers for each attribute/column.
+
+        Creates a table with the user supplied 'name' parameter (e.g. 'hospital').
+        """
+
         tic = time.clock()
         try:
+            # Load raw CSV file/data into the Postgres 'init_X' table.
+
             self.raw_data = Table(name, Source.FILE, f_path, f_name, na_values)
             # Add _tid_ column to dataset
             df = self.raw_data.df
             df.insert(0,'_tid_', range(0,len(df)))
             df.fillna('_nan_',inplace=True)
+            # Call to store to database
             self.raw_data.store_to_db(self.engine.engine)
             status = 'DONE Loading '+f_name
+
+            # Generate indexes on attribute columns for faster queries
+
             for attr in self.raw_data.get_attributes():
                 # Generate index on attribute
                 self.raw_data.create_db_index(self.engine,[attr])
+
+            # Create attr_to_idx dictionary (assign unique index for each attribute)
+            # and attr_number (total # of attributes)
+
             tmp_attr_list = self.raw_data.get_attributes()
             tmp_attr_list.remove('_tid_')
             for idx, attr in enumerate(tmp_attr_list):
                 # Map attribute to index
                 self.attr_to_idx[attr] = idx
             self.attr_number = len(self.raw_data.get_attributes())
+
         except Exception as e:
             status = ' '.join(['For table:', name, str(e)])
         toc = time.clock()
@@ -84,6 +112,21 @@ class Dataset:
         self.constraints = constraints
 
     def generate_aux_table(self, aux_table, df, store=False, index_attrs=False):
+        """
+        generate_aux_table writes/overwrites the auxiliary table specified by
+        'aux_table'.
+
+        It does:
+          1. stores/replaces the specified aux_table into Postgres (store=True), AND/OR
+          2. sets an index on the aux_table's internal Pandas DataFrame (index_attrs=[<columns>]), AND/OR
+          3. creates Postgres indexes for aux_table (store=True and index_attrs=[<columns>])
+
+        :param aux_table: (str) name of auxiliary table (see AuxTables)
+        :param df: (DataFrame) dataframe to memoize/store for this auxiliary table
+        :param store: (bool) if true, creates/replaces Postgres table for this auxiliary table
+        :param index_attrs: (list[str]) list of attributes to create indexes on. If store is true,
+        also creates indexes on Postgres table.
+        """
         try:
             self.aux_table[aux_table] = Table(aux_table.name, Source.DF, df)
             if store:
@@ -105,6 +148,9 @@ class Dataset:
             raise Exception(' '.join(['For table:',aux_table.name,str(e)]))
 
     def get_raw_data(self):
+        """
+        Is this guaranteed sorted by TID?
+        """
         if self.raw_data:
             return self.raw_data.df
         else:
@@ -119,6 +165,11 @@ class Dataset:
             raise Exception('ERROR No dataset loaded')
 
     def get_cell_id(self, tuple_id, attr_name):
+        """
+        get_cell_id returns cell ID: a unique ID for every cell.
+
+        Cell ID: _tid_ * (# of attributes) + attr_idx
+        """
         vid = tuple_id*self.attr_number + self.attr_to_idx[attr_name]
 
         return vid
@@ -131,9 +182,23 @@ class Dataset:
         return stats
 
     def collect_stats(self):
+        """
+        collect_stats memoizes:
+          1. self.single_attr_stats ({ attribute -> Series (value -> count) })
+            the frequency (# of entities) of a given attribute-value
+          2. self.pair_attr_stats ({ attr1 -> { attr2 -> DataFrame } } where
+            DataFrame contains 3 columns:
+              <attr1>: all possible values for attr1 ('val1')
+              <attr2>: all values for attr2 that appeared at least once with <val1> ('val2')
+              <count>: frequency (# of entities) where attr1: val1 AND attr2: val2
+            Also known as co-occurrence count.
+        """
+
         self.total_tuples = self.get_raw_data().shape[0]
+        # Single attribute-value frequency
         for attr in self.get_attributes():
             self.single_attr_stats[attr] = self.get_stats_single(attr)
+        # Co-occurence frequency
         for cond_attr in self.get_attributes():
             self.pair_attr_stats[cond_attr] = {}
             for trg_attr in self.get_attributes():
@@ -141,14 +206,27 @@ class Dataset:
                     self.pair_attr_stats[cond_attr][trg_attr] = self.get_stats_pair(cond_attr,trg_attr)
 
     def get_stats_single(self, attr):
+        """
+        Returns a Series indexed on possible values for 'attr' and contains the frequency.
+        """
         tmp_df = self.get_raw_data()[[attr]].groupby([attr]).size()
         return tmp_df
 
     def get_stats_pair(self, cond_attr, trg_attr):
+        """
+        Returns a DataFrame containing 3 columns:
+            <cond_attr>: all possible values for cond_attr ('val1')
+            <trg_attr>: all values for trg_attr that appeared at least once with <val1> ('val2')
+            <count>: frequency (# of entities) where cond_attr: val1 AND trg_attr: val2
+        """
         tmp_df = self.get_raw_data()[[cond_attr,trg_attr]].groupby([cond_attr,trg_attr]).size().reset_index(name="count")
         return tmp_df
 
     def get_domain_info(self):
+        """
+        Returns (number of random variables, count of distinct values across all attributes).
+        """
+
         query = 'SELECT count(_vid_), max(domain_size) FROM %s'%AuxTables.cell_domain.name
         res = self.engine.execute_query(query)
         total_vars = int(res[0][0])
--- repair/featurize/occurfeat.py
+++ repair/featurize/occurfeat.py
@@ -23,6 +23,15 @@ class OccurFeaturizer(Featurizer):
         self.setup_stats()
 
     def setup_stats(self):
+        """
+        Memoize single (frequency of attribute-value) and
+        pairwise stats (frequency of attr1-value1-attr2-value2)
+        from Dataset.
+
+        self.single_stats is a dict { attribute -> { value -> count } }.
+        self.pair_stats is a dict { attr1 -> { attr2 -> { val1 -> {val2 -> co-occur frequency } } } }.
+        """
+        # raw_data_dict is a Dictionary mapping TID -> { attribute -> value }
         self.raw_data_dict = self.ds.raw_data.df.set_index('_tid_').to_dict('index')
         total, single_stats, pair_stats = self.ds.get_statistics()
         self.total = float(total)
@@ -39,6 +48,16 @@ class OccurFeaturizer(Featurizer):
                 self.pair_stats[attr1][attr2] = dictify(tmp_df)
 
     def create_tensor(self):
+        """
+        For each unique VID (cell) returns the co-occurrence probability between
+        each possible domain value for this VID and the initial/raw values for the
+        corresponding entity/tuple of this cell.
+
+        :return: Torch.Tensor of shape (# of VIDs) X (max domain) X (# of attributes)
+            where tensor[i][j][k] contains the co-occur probability between the j-th domain value
+            of the i-th random variable (VID) and the initial/raw value of the k-th
+            attribute for the corresponding entity.
+        """
         # Iterate over tuples in domain
         tensors = []
         # Set tuple_id index on raw_data
@@ -55,28 +74,53 @@ class OccurFeaturizer(Featurizer):
         return combined
 
     def gen_feat_tensor(self, row, tuple):
+        """
+        For a given cell, we calculate the co-occurence probability of all domain values
+        for row['attribute'] with the row's co-value in every co-attributes.
+
+        That is for a domain value 'd' and current co-attribute value 'c' we have
+            P(d | c)   P(d, c) / P(c)
+
+        where P(d,c) is the empirical co-occurrence frequency of 'd' and 'c' and P(c)
+        is the frequency of 'c'.
+        """
+        # tensor is a (1 X domain size X # of attributes) pytorch.Tensor
+        # tensor[0][domain_idx][coattr_idx] contains the co-occurrence probability
+        # between the co-attribute (coattr_idx) and the domain values
+        # a possible domain value for this entity (row/tuple)
         tensor = torch.zeros(1, self.classes, self.attrs_number)
         rv_attr = row['attribute']
+        # Domain value --> index mapping
         domain = row['domain'].split('|||')
         rv_domain_idx = {val: idx for idx, val in enumerate(domain)}
+
+        # Iterate through every attribute (and current value for that
+        # attribute) and set the co-occurrence probability for every
+        # domain value for our current row['attribute'].
         for attr in self.all_attrs:
-            if attr != rv_attr and (not pd.isnull(tuple[attr])):
-                attr_idx = self.ds.attr_to_idx[attr]
-                val = tuple[attr]
-                count1 = float(self.single_stats[attr][val])
-                # Get topK values
-                if val not in self.pair_stats[attr][rv_attr]:
-                    if not pd.isnull(tuple[rv_attr]):
-                        raise Exception('Something is wrong with the pairwise statistics. <Val> should be present in dictionary.')
+            # Skip pairwise with current attribute or NULL value
+            if attr == rv_attr or pd.isnull(tuple[attr]):
+                continue
+
+            attr_idx = self.ds.attr_to_idx[attr]
+            val = tuple[attr]
+            attr_freq = float(self.single_stats[attr][val])
+            # Get topK values
+            if val not in self.pair_stats[attr][rv_attr]:
+                if not pd.isnull(tuple[rv_attr]):
+                    raise Exception('Something is wrong with the pairwise statistics. <{val}> should be present in dictionary.'.format(val))
+            else:
+                # dict of { val -> co-occur count }
+                all_vals = self.pair_stats[attr][rv_attr][val]
+                if len(all_vals) <= len(rv_domain_idx):
+                    candidates = list(all_vals.keys())
                 else:
-                    all_vals = self.pair_stats[attr][rv_attr][val]
-                    if len(all_vals) <= len(rv_domain_idx):
-                        candidates = all_vals
-                    else:
-                        candidates = domain
-                    for rv_val in candidates:
-                        count2 = float(all_vals.get(rv_val,0.0))
-                        prob = count2/count1
-                        if rv_val in rv_domain_idx:
-                            tensor[0][rv_domain_idx[rv_val]][attr_idx] = prob
+                    candidates = domain
+
+                # iterate through all possible domain values of row['attribute']
+                for rv_val in candidates:
+                    count2 = float(all_vals.get(rv_val,0.0))
+                    prob = count2/attr_freq
+                    if rv_val in rv_domain_idx:
+                        tensor[0][rv_domain_idx[rv_val]][attr_idx] = prob
         return tensor
