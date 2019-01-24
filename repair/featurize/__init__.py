0ac94d03604241325881f79f0b0bfb5ecb7dc76b
Mina Farid <mina.farid@uwaterloo.ca>
2019-01-24 12:19:10 -0500

    Removed deprecated OccurFeaturizer

--- repair/featurize/__init__.py
+++ repair/featurize/__init__.py
@@ -3,11 +3,10 @@ from .featurizer import Featurizer
 from .initfeat import InitFeaturizer
 from .initsimfeat import InitSimFeaturizer
 from .freqfeat import FreqFeaturizer
-from .occurfeat import OccurFeaturizer
 from .constraintfeat import ConstraintFeat
 from .langmodel import LangModelFeat
 from .initattfeat import InitAttFeaturizer
 from .occurattrfeat import OccurAttrFeaturizer
 
 __all__ = ['FeaturizedDataset', 'Featurizer', 'InitFeaturizer', 'InitSimFeaturizer', 'FreqFeaturizer',
-           'OccurFeaturizer', 'ConstraintFeat', 'LangModelFeat', 'InitAttFeaturizer', 'OccurAttrFeaturizer']
+           'ConstraintFeat', 'LangModelFeat', 'InitAttFeaturizer', 'OccurAttrFeaturizer']
