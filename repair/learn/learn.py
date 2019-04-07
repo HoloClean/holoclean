9c37148bfbe63b9aa6b1a994efc2de2b36254ddd
Richard Wu <richardwu1997@gmail.com>
2019-04-07 15:57:14 -0400

    Support multiple layers during repair and made TupleEmbedding dump/load more sophisticated.

--- repair/learn/learn.py
+++ repair/learn/learn.py
@@ -1,12 +1,9 @@
 import logging
-import math
 
 import torch
 from torch import optim
-from torch.autograd import Variable
-from torch.nn import Parameter, ParameterList
+from torch.nn import Parameter, ParameterList, ReLU
 from torch.nn.functional import softmax
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tqdm import tqdm
 import numpy as np
 
@@ -15,104 +12,143 @@ class TiedLinear(torch.nn.Module):
     """
     TiedLinear is a linear layer with shared parameters for features between
     (output) classes that takes as input a tensor X with dimensions
-        (batch size) X (output_dim) X (in_features)
+        (batch size) X (max_domain) X (total # of features)
         where:
-            output_dim is the desired output dimension/# of classes
-            in_features are the features with shared weights across the classes
+            max_domain is the desired output dimension/# of classes
     """
 
-    def __init__(self, env, feat_info, output_dim, bias=False):
+    def __init__(self, env, feat_info, max_domain, layer_sizes, bias=False):
+        """
+        feat_info (list[FeatInfo]): list of FeatInfo namedtuples for each
+            featurizer
+        max_domain (int): number of domain values (e.g. max domain)
+        bias (bool): use bias on the first layer per feature.
+        layer_sizes (list[int]): Output size of each linear layer. Last layer
+            should have an output size of 1. E.g. [200, 1].
+        """
+        assert layer_sizes and layer_sizes[-1] == 1
+
         super(TiedLinear, self).__init__()
         self.env = env
-        # Init parameters
-        self.in_features = 0.0
-        self.weight_list = ParameterList()
-        if bias:
-             self.bias_list = ParameterList()
-        else:
-             self.register_parameter('bias', None)
-        self.output_dim = output_dim
+        self.act = ReLU()
+
+        self.max_domain = max_domain
         self.bias_flag = bias
-        # Iterate over featurizer info list
-        for feat_entry in feat_info:
-            learnable = feat_entry.learnable
-            feat_size = feat_entry.size
-            init_weight = feat_entry.init_weight
-            self.in_features += feat_size
-            feat_weight = Parameter(init_weight*torch.ones(1, feat_size), requires_grad=learnable)
-            if learnable:
-                self.reset_parameters(feat_weight)
-            self.weight_list.append(feat_weight)
-            if bias:
-                feat_bias = Parameter(torch.zeros(1, feat_size), requires_grad=learnable)
-                if learnable:
-                    self.reset_parameters(feat_bias)
-                self.bias_list.append(feat_bias)
 
-    def reset_parameters(self, tensor):
-        stdv = 1. / math.sqrt(tensor.size(0))
-        tensor.data.uniform_(-stdv, stdv)
+        # Create first layer: this layer is special since some weights
+        # cannot be learned.
+        self.first_layer_weights = ParameterList()
+        for feat in feat_info:
+            weight = Parameter(feat.init_weight*torch.ones(feat.size,
+                                                           layer_sizes[0]),
+                               requires_grad=feat.learnable)
+            if feat.learnable:
+                torch.nn.init.xavier_uniform_(weight)
+            self.first_layer_weights.append(weight)
 
-    def concat_weights(self):
-        self.W = torch.cat([t for t in self.weight_list],-1)
-        # Normalize weights.
-        if self.env['weight_norm']:
-            self.W = self.W.div(self.W.norm(p=2))
-        # expand so we can do matrix multiplication with each cell and their max # of domain values
-        self.W = self.W.expand(self.output_dim, -1)
         if self.bias_flag:
-            self.B = torch.cat([t.expand(self.output_dim, -1) for t in self.bias_list],-1)
+            self.first_layer_bias = Parameter(torch.zeros(1, sum(f.size for f in feat_info)))
+            torch.nn.init.xavier_uniform_(self.first_layer_bias)
+
+        # Create subsequent layers.
+        self.other_weights = ParameterList()
+        self.other_biases = ParameterList()
+        for in_dim, out_dim in zip(layer_sizes[:-1], layer_sizes[1:]):
+            weight = Parameter(torch.zeros(in_dim, out_dim))
+            # (max_domain, out_dim)
+            bias = Parameter(torch.zeros(1, out_dim))
+
+            # Randomly initialize weights.
+            torch.nn.init.xavier_uniform_(weight)
+            torch.nn.init.xavier_uniform_(bias)
+
+            self.other_weights.append(weight)
+            self.other_biases.append(bias)
+
+        logging.debug("training model with first layer size: %s",
+                      list(map(str, [w.shape for w in self.first_layer_weights])))
+        if len(self.other_weights):
+            logging.debug("training model with additional hidden layers of size: %s",
+                          list(map(str, [w.shape for w in self.other_weights])))
 
     def forward(self, X, index, mask):
-        # Concatenates different featurizer weights - need to call during every pass.
-        self.concat_weights()
-        output = X.mul(self.W)
+        """
+        Performs one forward pass and outputs the logits of size
+        (batch, max_domain)
+
+        X: (batch, # of classes, total # of features)
+        index: (batch)
+        mask: (batch, # of classes)
+        """
+        # Multiply through the first layer.
+        # (batch, # of classes, layers_size[0])
+        output = X.matmul(torch.cat([t for t in self.first_layer_weights],
+                                    dim=0))
         if self.bias_flag:
-            output += self.B
-        output = output.sum(2)
+            output.add_(self.first_layer_bias.expand(self.max_domain, -1))
+
+        for idx, (weight, bias) in enumerate(zip(self.other_weights,
+                                                 self.other_biases)):
+            # Apply activation on all but last layer.
+            output = self.act(output)
+            # (batch, # of classes, in_dim) --> (batch, # of classes, out_dim)
+            output = output.matmul(weight) + bias.expand(self.max_domain, -1)
+        # output should now be (batch, # of classes, 1)
+
+        # (batch, # of classes)
+        output = output.squeeze(-1)
+
         # Add our mask so that invalid domain classes for a given variable/VID
         # has a large negative value, resulting in a softmax probability
         # of de facto 0.
+        # (batch, # of classes)
         output.index_add_(0, index, mask)
         return output
 
 
 class RepairModel:
 
-    def __init__(self, env, feat_info, output_dim, bias=False):
+    def __init__(self, env, feat_info, max_domain, bias=False, layer_sizes=[1]):
+        """
+        feat_info (list[FeatInfo]): featurizer information
+        max_domain (int): maximum domain size i.e. output dimension
+        """
         self.env = env
-        # A list of tuples (name, is_featurizer_learnable, featurizer_output_size, init_weight, feature_names (list))
         self.feat_info = feat_info
-        self.output_dim = output_dim
-        self.model = TiedLinear(self.env, feat_info, output_dim, bias)
+        self.max_domain = max_domain
+        self.model = TiedLinear(self.env, feat_info, max_domain,
+                                bias=bias, layer_sizes=layer_sizes)
         self.featurizer_weights = {}
 
     def fit_model(self, X_train, Y_train, mask_train):
-        n_examples, n_classes, n_features = X_train.shape
-
+        """
+        X_train: (batch, # of classes (domain size), total # of features)
+        Y_train: (batch, 1)
+        mask_train: (batch, # of classes)
+        """
         loss = torch.nn.CrossEntropyLoss()
-        trainable_parameters = filter(lambda p: p.requires_grad, self.model.parameters())
+        trainable_parameters = filter(lambda p: p.requires_grad,
+                                      self.model.parameters())
         if self.env['optimizer'] == 'sgd':
-            optimizer = optim.SGD(trainable_parameters, lr=self.env['learning_rate'], momentum=self.env['momentum'],
+            optimizer = optim.SGD(trainable_parameters,
+                                  lr=self.env['learning_rate'],
+                                  momentum=self.env['momentum'],
                                   weight_decay=self.env['weight_decay'])
         else:
-            optimizer = optim.Adam(trainable_parameters, lr=self.env['learning_rate'], weight_decay=self.env['weight_decay'])
-
-        # lr_sched = ReduceLROnPlateau(optimizer, 'min', verbose=True, eps=1e-5, patience=5)
+            optimizer = optim.Adam(trainable_parameters,
+                                   lr=self.env['learning_rate'],
+                                   weight_decay=self.env['weight_decay'])
 
         batch_size = self.env['batch_size']
         epochs = self.env['epochs']
-        for i in tqdm(range(epochs)):
+        for epoch_idx in tqdm(range(1, epochs + 1)):
             cost = 0.
-            num_batches = n_examples // batch_size
+            num_batches = X_train.shape[0] // batch_size
             for k in range(num_batches):
                 start, end = k * batch_size, (k + 1) * batch_size
-                cost += self.__train__(loss, optimizer, X_train[start:end], Y_train[start:end], mask_train[start:end])
-
-            # Y_pred = self.__predict__(X_train, mask_train)
-            # train_loss = loss.forward(Y_pred, Variable(Y_train, requires_grad=False).squeeze(1))
-            # logging.debug('overall training loss: %f', train_loss)
-            # lr_sched.step(train_loss)
+                cost += self.__train__(loss, optimizer, X_train[start:end],
+                                       Y_train[start:end],
+                                       mask_train[start:end])
 
             if self.env['verbose']:
                 # Compute and print accuracy at the end of epoch
@@ -120,9 +156,8 @@ class RepairModel:
                 Y_pred = self.__predict__(X_train, mask_train)
                 Y_assign = Y_pred.data.numpy().argmax(axis=1)
                 logging.debug("Epoch %d, cost = %f, acc = %.2f%%",
-                        i + 1, cost / num_batches,
-                        100. * np.mean(Y_assign == grdt))
-
+                              epoch_idx, cost / num_batches,
+                              100. * np.mean(Y_assign == grdt))
 
     def infer_values(self, X_pred, mask_pred):
         logging.info('inferring on %d examples (cells)', X_pred.shape[0])
@@ -130,45 +165,55 @@ class RepairModel:
         return output
 
     def __train__(self, loss, optimizer, X_train, Y_train, mask_train):
-        X_var = Variable(X_train, requires_grad=False)
-        Y_var = Variable(Y_train, requires_grad=False)
-        mask_var = Variable(mask_train, requires_grad=False)
+        """
+        X_train: (batch, # of classes (domain size), total # of features)
+        Y_train: (batch, 1)
+        mask_train: (batch, # of classes)
+        """
+        index = torch.LongTensor(range(X_train.size()[0]))
 
-        index = torch.LongTensor(range(X_var.size()[0]))
-        index_var = Variable(index, requires_grad=False)
-
-        optimizer.zero_grad()
         # Fully-connected layer with shared parameters between output classes
         # for linear combination of input features.
         # Mask makes invalid output classes have a large negative value so
         # to zero out softmax probability.
-        fx = self.model.forward(X_var, index_var, mask_var)
-        # loss is CrossEntropyLoss: combines log softmax + Negative log likelihood loss.
+        fx = self.model.forward(X_train, index, mask_train)
+        # loss is CrossEntropyLoss: combines log softmax + Negative log
+        # likelihood loss.
         # Y_Var is just a single 1D tensor with value (0 - 'class' - 1) i.e.
         # index of the correct class ('class' = max domain)
-        # fx is a tensor of length 'class' the linear activation going in the softmax.
-        output = loss.forward(fx, Y_var.squeeze(1))
+        # fx is a tensor of length 'class' the linear activation going in the
+        # softmax.
+        output = loss.forward(fx, Y_train.squeeze(1))
+
+        optimizer.zero_grad()
         output.backward()
         optimizer.step()
+
         cost = output.item()
         return cost
 
     def __predict__(self, X_pred, mask_pred):
-        X_var = Variable(X_pred, requires_grad=False)
-        index = torch.LongTensor(range(X_var.size()[0]))
-        index_var = Variable(index, requires_grad=False)
-        mask_var = Variable(mask_pred, requires_grad=False)
-        fx = self.model.forward(X_var, index_var, mask_var)
+        """
+        X_pred: (batch, # of classes (domain size), total # of features)
+        Y_pred: (batch, 1)
+        """
+        index = torch.LongTensor(range(X_pred.size()[0]))
+        fx = self.model.forward(X_pred, index, mask_pred)
         output = softmax(fx, 1)
         return output
 
     def get_featurizer_weights(self, feat_info):
         report = ""
         for i, f in enumerate(feat_info):
-            this_weight = self.model.weight_list[i].data.numpy()[0]
-            weight_str = "\n".join("{name} {weight}".format(name=name, weight=weight)
+            # TODO: fix this since we now have 200 weights (second layer size)
+            # per feature
+            this_weight = self.model.first_layer_weights[i].data\
+                .numpy().mean(axis=1)
+            weight_str = "\n".join("{name} {weight}".format(name=name,
+                                                            weight=weight)
                                    for name, weight in
-                                   zip(f.feature_names, map(str, np.around(this_weight, 3))))
+                                   zip(f.feature_names,
+                                       map(str, np.around(this_weight, 3))))
             feat_name = f.name
             feat_size = f.size
             max_w = max(this_weight)
