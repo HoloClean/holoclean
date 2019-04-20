bd319bfd86851786e159a0b412acfc838bf21b28
Richard Wu <richardwu1997@gmail.com>
2019-04-19 23:24:11 -0400

    Support specifying n-dimensional numerical attr groups vs splitting on columns.

--- dataset/quantization.py
+++ dataset/quantization.py
@@ -5,39 +5,44 @@ from sklearn.cluster import KMeans
 from utils import NULL_REPR
 
 
-def quantize_km(env, df_raw, bin_number_dict):
+def quantize_km(env, df_raw, num_attr_groups_bins):
     """
     Kmeans clustering using sklearn
     https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
     Currently do 1D clustering
     :param df_raw: pandas.dataframe
-    :param bin_number_dict: a dict existing requested number of bins for each numerical attrs,
-    the numerical attrs not in this dict will not do quantization
+    :param num_attr_groups_bins: list[tuple] where each tuple consists of
+    (# of bins, list[str]) where the list[str] is a group of attribues to be
+    treated as numerical.
+    Groups must be disjoint.
+
     :return: pandas.dataframe after quantization
     """
     tic = time.time()
     df_quantized = df_raw.copy()
 
-    for col in bin_number_dict.keys():
-        df_col = df_quantized.loc[df_quantized[col] != NULL_REPR, [col]].reset_index(drop=True)
-        # Matrix of possibly n-dimension values
-        X_col = np.array(list(map(lambda v: v.split(env['numerical_sep']), df_col[col].values)),
-                         dtype=np.float32)
+    # Assert groups are disjoint
+    num_attrs = [attr for _, group in num_attr_groups_bins for attr in group]
+    assert len(set(num_attrs)) == len(num_attrs)
 
-        bin_number = bin_number_dict[col]
-        n_clusters = min(bin_number, np.unique(X_col, axis=0).shape[0])
+    for bins, attrs in num_attr_groups_bins:
+        fil_notnull = (df_quantized[attrs] != NULL_REPR).all(axis=1)
+
+        df_group = df_quantized.loc[fil_notnull, attrs].reset_index(drop=True)
+        # Matrix of possibly n-dimension values
+        X_attrs = df_group.values.astype(np.float)
+
+        n_clusters = min(bins, np.unique(X_attrs, axis=0).shape[0])
 
         km = KMeans(n_clusters=n_clusters)
-        km.fit(X_col)
+        km.fit(X_attrs)
 
         label_pred = km.labels_
         centroids = km.cluster_centers_
 
-        # Lookup cluster centroids and concatenate their values again with
-        # env['numerical_sep'].
-        def quantize_row(row):
-            return env['numerical_sep'].join(map(str, centroids[label_pred[row.name]]))
-        df_quantized.loc[df_quantized[col] != NULL_REPR, col] = df_col.apply(quantize_row, axis=1).values
+        # Lookup cluster centroids and replace their values.
+        df_quantized.loc[fil_notnull, attrs] = np.array([centroids[label_pred[idx]]
+            for idx in df_group.index]).astype(str)
 
     status = "DONE with quantization"
     toc = time.time()
