5f9d1069ec36c0d8c8ddb19996273502a44995b3
Trung Huynh <trunghlt@gmail.com>
2020-10-09 13:51:20 +0100

    Replaced database auth with sqlachlemy_uri instead of username and passowrd

--- examples/holoclean_repair_example.py
+++ examples/holoclean_repair_example.py
@@ -4,10 +4,7 @@ from holoclean.repair.featurize import *
 
 # 1. Setup a HoloClean session.
 hc = holoclean.HoloClean(
-    db_name='superset',
-    db_pwd='superset',
-    db_host='localhost',
-    db_user='superset',
+    sqlalchemy_uri="postgresql://superset:superset@localhost:5432/superset",
     domain_thresh_1=0.0,
     domain_thresh_2=0.0,
     weak_label_thresh=0.99,
